cb9b1fc6cbc3c39178113a005ee292d3f45696df
jwtylr <jw05taylor@gmail.com>
2025-12-23 17:17:07 -0500

    Added YouTube Webcams

--- smuggs.php
+++ smuggs.php
@@ -8,7 +8,17 @@
     <h1>Smuggs</h1>
     <h2>Weather</h2>
     <h2>Info</h2>
+    <h3>Address:</h3><body>4323 VT-108, Jeffersonville, VT 05464</body>
+
     <h2>Resort Webcam</h2>
+    <iframe width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/HOhniNpNFZg?si=WEb-PvUBQ0tY4ogi" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+            allowfullscreen></iframe>
     <h2>Traffic Cameras</h2>
 </body>
 </html>
