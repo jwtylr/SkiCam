95998e24f05625794bd70143a6767c714132ed19
jwtylr <jw05taylor@gmail.com>
2025-12-23 15:54:52 -0500

    Refactored index

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>SkiCam</title>
+</head>
+<body>
+    <?php include 'nav.php'; ?>
+    <h1>Welcome to SkiCam!</h1> 
+    <body>This is an online tool that hosts resort livestreams alongside traffic cameras around Northern Vermont ski resorts. Developed by Jack Taylor at the University of Vermont.</body>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <a href="smuggs.html">Smuggs</a>
+    <a href="bolton.html">Bolton</a>
+    <a href="stowe.html">Stowe</a>
+    <a href="bush.html">Bush</a>
+    <a href="kmart.html">K-Mart</a>
+    <a href="jay.html">Jay</a>
+</nav>
