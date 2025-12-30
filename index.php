f4b20ef77d85fda89c1f13324a4eb38948da126c
jwtylr <jw05taylor@gmail.com>
2025-12-29 22:16:09 -0500

    Updated todo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <body>This is an online tool that hosts resort livestreams alongside traffic cameras around Northern Vermont ski resorts. Developed by Jack Taylor at the University of Vermont.</body>
 <h3>To Do's:</h3>
 <ul>
-    <li>Make cache improvements (actual file-based cache)</li>
+    <li>Make cache improvements (actual file-based cache, 100x network efficiency)</li>
     <li>Improve responsive design for mobile devices</li>
     <li>User comments for conditions reports?</li>
     <li>Additional resorts/cams</li>
